b64abe01c81311c64a686cf627ba4d137d013fb9
Sean Kennedy <cspheregreen@gmail.com>
2017-05-12 11:22:09 -0500

    pg

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 ruby '2.3.1'
 
 gem 'rails', '~> 5.0.2'
-gem 'sqlite3'
+gem 'pg'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170506124459) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -27,8 +30,8 @@ ActiveRecord::Schema.define(version: 20170506124459) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
