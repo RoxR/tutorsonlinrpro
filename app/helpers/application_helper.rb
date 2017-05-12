9590946dbe13eed49a8d850f437cd29e85d0c13d
Sean Kennedy <cspheregreen@gmail.com>
2017-05-12 14:27:12 -0500

    courses styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def body_tag(&block)
+    content = capture(&block)
+    cls = params[:controller].gsub(/\//, '-')
+    content_tag :body, content, class: cls
+  end
 end
+
