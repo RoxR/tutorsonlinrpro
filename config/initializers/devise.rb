33cd9e108b4fc6f44cc294072b7d628235d0dabb
Sean Kennedy <cspheregreen@gmail.com>
2017-05-12 11:29:18 -0500

    temporarily comment out mailer_sender for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
+  #
+  # TODO: https://github.com/RailsApps/rails-devise-pundit/issues/8
+  # config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
